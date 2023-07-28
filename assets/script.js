af53f5d3aa0377adc9eaa1322129a43268b5a3c5
Linh Lien <lienx149@gmail.com>
2023-07-28 18:33:17 -0500

    added history functionality

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ let inputEl = document.querySelector(".textinput");
 let dropDownEl = document.querySelector(".ddstate")
 let loc = "";
 let searchedCities = [];
-
+let mapEl = document.querySelector('#map');
 function InitiateBreweryUI(data) {
   const brewerySectionDiv = document.getElementById("brewerySection");
 
@@ -76,10 +76,12 @@ function brew() {
       })
       .then(function (data) {
         if (data) {
-          console.log(data);
+          // console.log(data);
 
           InitiateBreweryUI(data);
+          
         }
+        
         // getMap(data);
         var mapDrawLocations = [];
         var breweryNames = [];
@@ -90,17 +92,20 @@ function brew() {
           var nameConcate = data[i].name;
           breweryNames.push(nameConcate.concat());
           
-
+          inputEl.textContent = ''; 
           // mapDrawLocations.push(subArrayTwo.concat());
         }
         
-        console.log(mapDrawLocations);
-        console.log(breweryNames);
-        
+        // console.log(mapDrawLocations);
+        // console.log(breweryNames);
+      
         //
+       
         MQ.geocode()
           .search(mapDrawLocations)
           .on("success", function (e) {
+           
+
             var results = e.result,
               html = "",
               group = [],
@@ -114,11 +119,12 @@ function brew() {
               map,
               r,
               i;
-
+              
             map = L.map("map", {
+              
               layers: MQ.mapLayer(),
             });
-
+           
             for (i = 0; i < results.length; i++) {
               result = results[i].best;
               latlng = result.latlng;
@@ -156,22 +162,34 @@ function brew() {
             features = L.featureGroup(group).addTo(map);
             map.fitBounds(features.getBounds());
           });
-        //
+          
         //drawMap();
         //set search history to array
         var city = inputEl.value;
-        console.log(city);
+        let state = dropDownEl.value;
+        // console.log(city);
         var data = localStorage.getItem("searchedCities");
+        let dataS = localStorage.getItem("searchedStates")
         var searchedCities = data ? JSON.parse(data) : [];
+        let searchedStates = dataS ? JSON.parse(dataS) : [];
         searchedCities.push(city);
+        searchedStates.push(state);
         //filter search history to prevent duplicates then set to local storage
         let filteredSearchHistory = searchedCities.filter((element, index) => {
           return searchedCities.indexOf(element) === index;
         });
+        let filteredSearchHistoryState = searchedStates.filter((element, index) => {
+          return searchedStates.indexOf(element) === index;
+        });
+        
         localStorage.setItem(
           "searchedCities",
           JSON.stringify(filteredSearchHistory)
         );
+        localStorage.setItem(
+          "searchedStates",
+          JSON.stringify(filteredSearchHistoryState)
+        )
 init()
       });
   } else {
@@ -183,47 +201,155 @@ buttonEl.addEventListener("click", brew);
 function init() {
   // load search history on page load
   var data = localStorage.getItem("searchedCities");
+  let dataS = localStorage.getItem('searchedStates');
   var searchedCities = data ? JSON.parse(data) : [];
-console.log(searchedCities);
+  let searchedStates = dataS ? JSON.parse(dataS) : [];
+
 let historyCitiesEl = document.querySelector(".historyCities")
 historyCitiesEl.textContent ='';
+
   if (searchedCities.value !== "") {
     for (i = 0; i < searchedCities.length; i++) {
       //add .append for each index of string here
       
       let divElh = document.createElement("div");
-      divElh.setAttribute('class','m-5')
-      divElh.setAttribute('id',searchedCities[i])
+      divElh.setAttribute('class','mx-5')
+      divElh.setAttribute('id',i)
+      divElh.setAttribute('onclick',"recall(" + i + ")")
       historyCitiesEl.appendChild(divElh)
-      divElh.textContent = searchedCities[i]
+      divElh.textContent = searchedCities[i] + ", " + searchedStates[i];
 
       divElh.addEventListener("click",function(){
-        console.log(this.textContent)
+        // console.log(this.textContent)
       })
     }
   }
-
-  
 
 }
 
 init()
 
-// let historyArray = [];
+function clearHist(){
+  let historyCitiesEl = document.querySelector(".historyCities")
+  historyCitiesEl.textContent ='';
+  localStorage.clear();
+  searchedCities = [];
 
-// function renderHistory() {
+  }
 
-// let historyEl = document.querySelector('#history');
 
-// // historyEl.push(inputEl.value);
-// for (let index = 0; index < historyArray.length; index++) {
-//   let hist = historyArray[index];
-// }
-// ViewScore.addEventListener('click', function (){
-  
-//   console.log(ViewScore);
-// });
-// }
+
+function recall(x){
+
+  var tata = JSON.parse(localStorage.getItem("searchedCities"));
+  let tataS = JSON.parse(localStorage.getItem('searchedStates'));
+  console.log(tata[x])
+  console.log(tataS[x])
+
+
+  if (tata[x] !== "") {
+    fetch(
+      `https://api.openbrewerydb.org/v1/breweries?by_city=${tata[x]}&by_state=${tataS[x]}&per_page=50`
+     
+    ) 
+      .then(function (response) {
+        
+        return response.json();
+      })
+      .then(function (data) {
+        if (data) {
+          console.log(data);
+
+          InitiateBreweryUI(data);
+          
+        }
+        
+        // getMap(data);
+        var mapDrawLocations = [];
+        var breweryNames = [];
+        for (i = 0; i < data.length; i++) {
+          if (data[i].address_1 == null){data[i].address_1 = ""}
+          var addressConcate = data[i].address_1.replace('#','') + " " + data[i].city + " " + data[i].state;
+          mapDrawLocations.push(addressConcate.concat());
+          var nameConcate = data[i].name;
+          breweryNames.push(nameConcate.concat());
+          
+          inputEl.textContent = ''; 
+          // mapDrawLocations.push(subArrayTwo.concat());
+        }
+        
+        console.log(mapDrawLocations);
+        console.log(breweryNames);
+      
+        //
+       
+        MQ.geocode()
+          .search(mapDrawLocations)
+          .on("success", function (e) {
+           
+
+            var results = e.result,
+              html = "",
+              group = [],
+              features,
+              marker,
+              result,
+              latlng,
+              prop,
+              best,
+              val,
+              map,
+              r,
+              i;
+              
+            map = L.map("map", {
+              
+              layers: MQ.mapLayer(),
+            });
+           
+            for (i = 0; i < results.length; i++) {
+              result = results[i].best;
+              latlng = result.latlng;
+              brewName = breweryNames[i];
+
+              html += '<div style="width:300px; float:left;">';
+              html +=
+                "<p><strong>Geocoded Location #" + (i + 1) + "</strong></p>";
+
+              for (prop in result) {
+                r = result[prop];
+
+                if (prop === "displayLatLng") {
+                  val = r.lat + ", " + r.lng;
+                } else if (prop === "mapUrl") {
+                  val = '<br /><img src="' + r + '" />';
+                } else {
+                  val = r;
+                }
+
+                html += prop + " : " + val + "<br />";
+              }
+
+              html += "</div>";
+
+              // create POI markers for each location
+              marker = L.marker([latlng.lat, latlng.lng]).bindPopup(
+                "<strong>" + brewName + "</strong>" + "</br>" + result.street
+              );
+
+              group.push(marker);
+            }
+
+            // add POI markers to the map and zoom to the features
+            features = L.featureGroup(group).addTo(map);
+            map.fitBounds(features.getBounds());
+          });
+ 
+init()
+      });
+  }
+}
+
 
 
 
